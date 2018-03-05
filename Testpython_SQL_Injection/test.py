ca74f026f5903715447ca450fea018505ce35769
Hyunkel <dhyunkel@gmail.com>
2018-03-05 14:39:09 +0700

    Update test.py

--- Testpython_SQL_Injection/test.py
+++ Testpython_SQL_Injection/test.py
@@ -17,14 +17,19 @@ class Demo_Test(unittest.TestCase, custom.CustomFun):
         for i in x:
             m = re.search(r"(?<==POKER).*?(?=', version)", i).group(0)
             listurl.append(m)
+        z =0
         for j in listurl:
+            z= z+1
+            print z
             url_test = url_main + j
             print url_test
             r = requests.get(url_test)
             # check data exist
             self.assertTrue(r.content)
+            print r.content
             data = ast.literal_eval(r.content)
             # check type data
             self.check_type_data(data, dict)
+            print ("======================================")
 if __name__ == "__main__":
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='./Result_xml'))
